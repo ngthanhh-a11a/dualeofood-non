7c101f90b65afc6528321dc7bf62d0fbd23e3ca6
ngthanhh-a11a <nguyenducthanh072005@gmail.com>
2026-08-04 23:47:52 +0700

    Fix sendEmail from address to be independent of SMTP Login

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -14,7 +14,7 @@ const sendEmail = async (options) => {
 
   // 2. Thiết lập nội dung email
   const mailOptions = {
-    from: `"DUALEOFOOD ADMIN" <${process.env.EMAIL_USER}>`,
+    from: `"DUALEOFOOD ADMIN" <nguynducthanh555@gmail.com>`, // Bắt buộc phải là email đã verify trên Brevo
     to: options.email,
     subject: options.subject,
     html: options.html, // Hỗ trợ gửi email bằng code HTML cho đẹp
